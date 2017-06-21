461d97db73c3c37be00dd6005f5afee556705318
wudtichaikarun <595159090004@dpu.ac.th>
2017-06-22 05:35:32 +0700

    can get data from db /profile success

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -10,12 +10,50 @@ export class AuthService {
 
   constructor(private http: Http ) { }
 
-  // Sent data to routes/users.js method post('/register', ....) like postMan
+  // Register Sent data to routes/users.js method post('/register', ....) like postMan
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
     return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
     .map(res => res.json());
+  }
+
+  // Authentication
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    .map(res => res.json());
+  }
+
+  // Profile get data
+  getProfile(){
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type','application/json');
+    return this.http.get('http://localhost:3000/users/profile',{headers: headers})
+    .map(res => res.json());
+  }
+
+  // Set local storage
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user
+  }
+
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  // Log out
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 
 }
