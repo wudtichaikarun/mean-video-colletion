9bd6b7325e4b148ce5a77dbe84dbd5c85fd724ea
wudtichaikarun <595159090004@dpu.ac.th>
2017-06-23 01:52:18 +0700

    app success

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -15,14 +15,32 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import {AuthGuard } from './guards/auth.guard';
 
 
 const appRoutes: Routes = [
- {path: '', component: HomeComponent},
- {path: 'register', component: RegisterComponent},
- {path: 'login', component: LoginComponent},
- {path: 'dashboard', component: DashboardComponent},
- {path: 'profile', component: ProfileComponent}
+ {
+   path: '', 
+   component: HomeComponent
+  },
+ {
+   path: 'register', 
+   component: RegisterComponent
+  },
+ {
+   path: 'login', 
+   component: LoginComponent
+  },
+ {
+   path: 'dashboard', 
+   component: DashboardComponent,
+   canActivate: [AuthGuard]
+  },
+ {
+   path: 'profile', 
+   component: ProfileComponent,
+   canActivate: [AuthGuard]
+  }
  
 ]
 
@@ -43,7 +61,11 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService,AuthService],
+  providers: [
+    ValidateService,
+    AuthService,
+    AuthGuard
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -48,6 +49,12 @@ export class AuthService {
     const token = localStorage.getItem('id_token');
     this.authToken = token;
   }
+
+
+  loggedIn() {
+  return tokenNotExpired('id_token');
+
+}
 
   // Log out
   logout(){
