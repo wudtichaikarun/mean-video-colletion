d926b47259b6fa264a903955b3667307001e4229
wudtichaikarun <595159090004@dpu.ac.th>
2017-06-24 21:20:41 +0700

    chang db 2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://romantic:123456@ds135522.mlab.com:35522/meanauthapp',
+    database: 'mongodb://ro:123456@ds135522.mlab.com:35522/meanauthapp',
     secret: 'romantic'
 }
